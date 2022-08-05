7b43afde9d645f95d7d31816145d22d8a41ed0d3
JuriaLee <snidel@naver.com>
2022-08-05 16:04:37 +0900

    v3

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-
 topics = [
-    {"id":1, "title":"commit", "view":100}
-    {"id":2, "title":"push", "view":200}
+    {"id":1, "title":"commit", "view":100},
+    {"id":2, "title":"push", "view":200},
     {"id":3, "title":"pull", "view":50}
 ]
-topics
